005f28d113dbe80031186792fac02370fbc6ef69
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-05-14 17:10:42 -0400

    Add test to verify blank monit file with whitespaces
    Signed-off-by: Michael Tang <mitang@pivotal.io>

--- main_test.go
+++ main_test.go
@@ -99,22 +99,22 @@ instance_groups:
   jobs:
   - name: check-monit
     release: {{.ReleaseName}}
+- name: check-monit-whitespace
+  instances: 1
+  stemcell: windows
+  lifecycle: errand
+  azs: [{{.AZ}}]
+  vm_type: {{.VmType}}
+  vm_extensions: [{{.VmExtensions}}]
+  networks:
+  - name: {{.Network}}
+  jobs:
+  - name: check-monit
+    release: {{.ReleaseName}}
+		properties:
+			check-monit:
+				content: "\t\n \t \n "
 `
-
-// - name: check-monit-whitespace
-//   instances: 1
-//   stemcell: windows
-//   lifecycle: errand
-//   azs: [{{.AZ}}]
-//   vm_type: {{.VmType}}
-//   vm_extensions: [{{.VmExtensions}}]
-//   networks:
-//   - name: {{.Network}}
-//   jobs:
-//   - name: check-monit
-//     release: {{.ReleaseName}}
-//     content: "\t\n \t \n "
-
 var rootDiskInstanceGroup = fmt.Sprintf(`
 - name: verify-root-disk-size
   instances: 1
