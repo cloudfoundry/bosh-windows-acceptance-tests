87dd167e915680557112585eedf506fbc65b0100
Gab Satchi <gsatchithanantham@pivotal.io>
2018-06-18 14:23:38 -0400

    WIP
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- assets/bwats-release/jobs/check-system/templates/run.ps1
+++ assets/bwats-release/jobs/check-system/templates/run.ps1
@@ -2,6 +2,10 @@
 # if we do not have permission to read file permissions.
 
 # Verify randomize password has run
+
+Test-LGPO()
+Test-LGPO()
+
 #potential
 $DisabledNetBIOS = $false
 $nbtstat = nbtstat.exe -n
@@ -271,51 +275,6 @@ if ($dataPartition -ne $null) {
     Exit 1
 }
 
-# Verify LGPO
-if ($windowsVersion -Match "2012") {
-  echo "Verifying that expected policies have been applied"
-
-  lgpo /b $PSScriptRoot
-  $LgpoDir = "$PSScriptRoot\" + (Get-ChildItem $PSScriptRoot -Directory | ?{ $_.Name -match "{*}" } | select -First 1).Name
-
-  $OutputDir="$PSScriptRoot\lgpo_test"
-  mkdir $OutputDir
-
-  lgpo /parse /m "$LgpoDir\DomainSysvol\GPO\Machine\registry.pol" > "$OutputDir\machine_registry.unedited.txt"
-  Get-Content "$OutputDir\machine_registry.unedited.txt" | select -Skip 3 > "$OutputDir\machine_registry.txt"
-
-  lgpo /parse /u "$LgpoDir\DomainSysvol\GPO\User\registry.pol" > "$OutputDir\user_registry.unedited.txt"
-  Get-Content "$OutputDir\user_registry.unedited.txt" | select -Skip 3 > "$OutputDir\user_registry.txt"
-
-  copy "$LgpoDir\DomainSysvol\GPO\Machine\microsoft\windows nt\Audit\audit.csv" "$OutputDir"
-  $Csv     = Import-Csv "$LgpoDir\DomainSysvol\GPO\Machine\microsoft\windows nt\Audit\audit.csv"
-  $Include = $Csv[0].psobject.properties | select -ExpandProperty Name -Skip 1
-  $Csv | select $Include | export-csv "$OutputDir\audit.csv" -NoTypeInformation
-
-  copy "$LgpoDir\DomainSysvol\GPO\Machine\microsoft\windows nt\SecEdit\GptTmpl.inf" "$OutputDir"
-
-  function Assert-NoDiff {
-    Param (
-      [string] $fileA = (Throw "first filename param required"),
-      [string] $fileB = (Throw "second filename param required")
-    )
-    fc.exe /t "$fileA" "$fileB"
-
-    if ($LastExitCode -ne 0) {
-      Write-Error "Expected no diff between $fileA and $fileB"
-      Exit 1
-    }
-  }
-
-  # Diff the files aginst the fixtures
-  $TestDir = "$PSScriptRoot\..\test"
-
-  Assert-NoDiff "$OutputDir\machine_registry.txt" "$TestDir\machine_registry.txt"
-  Assert-NoDiff "$OutputDir\user_registry.txt" "$TestDir\user_registry.txt"
-  Assert-NoDiff "$OutputDir\GptTmpl.inf" "$TestDir\GptTmpl.inf"
-  Assert-NoDiff "$OutputDir\audit.csv" "$TestDir\audit.csv"
-}
-
 echo "Verifying NTP synch works correctly"
 w32tm /query /configuration
 
@@ -362,6 +321,54 @@ if ($windowsVersion -Match "2012") {
   } else {
     Write-Host "IIS Web Server is turned off"
   }
+
 }
+
+# Verify LGPO
+function Test-LGPO {
+  if ($windowsVersion -Match "2012") {
+    echo "Verifying that expected policies have been applied"
+
+    lgpo /b $PSScriptRoot
+    $LgpoDir = "$PSScriptRoot\" + (Get-ChildItem $PSScriptRoot -Directory | ?{ $_.Name -match "{*}" } | select -First 1).Name
+
+    $OutputDir="$PSScriptRoot\lgpo_test"
+    mkdir $OutputDir
+
+    lgpo /parse /m "$LgpoDir\DomainSysvol\GPO\Machine\registry.pol" > "$OutputDir\machine_registry.unedited.txt"
+    Get-Content "$OutputDir\machine_registry.unedited.txt" | select -Skip 3 > "$OutputDir\machine_registry.txt"
+
+    lgpo /parse /u "$LgpoDir\DomainSysvol\GPO\User\registry.pol" > "$OutputDir\user_registry.unedited.txt"
+    Get-Content "$OutputDir\user_registry.unedited.txt" | select -Skip 3 > "$OutputDir\user_registry.txt"
+
+    copy "$LgpoDir\DomainSysvol\GPO\Machine\microsoft\windows nt\Audit\audit.csv" "$OutputDir"
+    $Csv     = Import-Csv "$LgpoDir\DomainSysvol\GPO\Machine\microsoft\windows nt\Audit\audit.csv"
+    $Include = $Csv[0].psobject.properties | select -ExpandProperty Name -Skip 1
+    $Csv | select $Include | export-csv "$OutputDir\audit.csv" -NoTypeInformation
+
+    copy "$LgpoDir\DomainSysvol\GPO\Machine\microsoft\windows nt\SecEdit\GptTmpl.inf" "$OutputDir"
+
+    function Assert-NoDiff {
+      Param (
+        [string] $fileA = (Throw "first filename param required"),
+        [string] $fileB = (Throw "second filename param required")
+      )
+      fc.exe /t "$fileA" "$fileB"
+
+      if ($LastExitCode -ne 0) {
+        Write-Error "Expected no diff between $fileA and $fileB"
+      }
+    }
+
+    # Diff the files aginst the fixtures
+    $TestDir = "$PSScriptRoot\..\test"
+
+    Assert-NoDiff "$OutputDir\machine_registry.txt" "$TestDir\machine_registry.txt"
+    Assert-NoDiff "$OutputDir\user_registry.txt" "$TestDir\user_registry.txt"
+    Assert-NoDiff "$OutputDir\GptTmpl.inf" "$TestDir\GptTmpl.inf"
+    Assert-NoDiff "$OutputDir\audit.csv" "$TestDir\audit.csv"
+  }
+}
+
 
 Exit 0
